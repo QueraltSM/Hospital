20917812360fc21096a58290dce1cd79cf13ec69
QueraltSM <queraltsosa@hotmail.com>
2017-06-24 19:28:49 +0100

    Create ContrataDoctorGUI.java

---
+++ src/ContrataDoctorGUI.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class ContrataDoctorGUI extends JDialog{
+    public ContrataDoctorGUI(JFrame f) {
+        super(f, "Contratar Doctor");
+        setVisible(true);
+        setSize(200, 200);
+        setLocationRelativeTo(f);
+    }
+}
