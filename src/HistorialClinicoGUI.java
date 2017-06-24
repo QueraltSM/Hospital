0d9f4707be27e8a535c893da997e101c3dcce1a8
QueraltSM <queraltsosa@hotmail.com>
2017-06-24 19:28:07 +0100

    Create HistorialClinicoGUI.java

---
+++ src/HistorialClinicoGUI.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+public class HistorialClinicoGUI extends JDialog {
+
+    public HistorialClinicoGUI(JFrame f) {
+        super(f, "Historial clínico");
+        setVisible(true);
+        setSize(200, 200);
+        setLocationRelativeTo(f);
+    }
+}
