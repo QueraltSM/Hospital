e11fb8f40227d42c616713a8878e0f323d5c0b76
QueraltSM <queraltsosa@hotmail.com>
2017-06-24 19:28:31 +0100

    Create RegistraPacienteGUI.java

---
+++ src/RegistraPacienteGUI.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+public class RegistraPacienteGUI extends JDialog {
+
+    public RegistraPacienteGUI(JFrame f) {
+        super(f, "Registrar Paciente");
+        setVisible(true);
+        setSize(200, 200);
+        setLocationRelativeTo(f);
+    }
+}
